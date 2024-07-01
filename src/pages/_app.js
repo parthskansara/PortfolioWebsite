6846fa24e1e5a0d4f865ddc5217e369d0dd84a52
unknown <parthskansara@gmail.com>
2024-07-01 15:47:04 -0700

    Added logo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -46,94 +46,98 @@ const App = ({Component, pageProps}) => {
           <meta name="Portfolio Website" content="A Snapshot of my entire Journey"/>
           <link rel="icon" href="/icons/favicon.png"/>
         </Head>
-
-        <NavigationMenu className="mt-8 ml-auto">
-          <NavigationMenuList className="gap-8 pr-8">
-            <NavigationMenuItem>
-              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/" && "border-2 border-black")} href="/">
-                <span className=''>home</span>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/resume" && "border-2 border-black")} href="/resume">
-                resume
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem className=''>
-              <NavigationMenuTrigger className={cn("text-lg py-4", (currentPath === "/experience" && "border-2 border-black"))}>
-                
-                {
-                  pathWithQuery === "/experience?type=education" ?
-                  "education" :
-                  (
-                    pathWithQuery === "/experience?type=work" ?
-                    "work" :
-                    (
-                      pathWithQuery === "/experience?type=positions" ?
-                      "extracurricular":
-                      "experience"
-                    )
-                  )
-
-                }
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className="p-4 bg-white shadow-lg rounded-md">
-                <ul className=''>
-                  <li>
-                    <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=education" && "border-2 border-black")} href="/experience?type=education">
-                      education
-                    </NavigationMenuLink>
-                  </li>
-                  <li>
-                    <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=work" && "border-2 border-black")} href="/experience?type=work">
-                      work  
-                    </NavigationMenuLink>
-                  </li>
-                  <li key="3">
-                    <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=positions" && "border-2 border-black")} href="/experience?type=positions">
-                      extracurricular
-                    </NavigationMenuLink>
-                  </li>
-                </ul>
-              </NavigationMenuContent>
-                  {/* <div className='flex items-center'>
-                    <span className='text-sm text-left block'>work</span>
-                    <Separator className='ml-2' />
-                  </div> */}
-                  {/* <span className='text-sm pl-2'>work <Separator/></span> */}
+        <div className='flex justify-between items-center mt-4'>
+          <Link href="/">
+            <img className="h-[50px] ml-8" src="/logo/Logo-2.png" alt="Parth Kansara - Logo" />
+          </Link>          
+          <NavigationMenu className="">          
+            <NavigationMenuList className="gap-8 pr-8">
+              <NavigationMenuItem>
+                <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/" && "border-2 border-black")} href="/">
+                  <span className=''>home</span>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/resume" && "border-2 border-black")} href="/resume">
+                  resume
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem className=''>
+                <NavigationMenuTrigger className={cn("text-lg py-4", (currentPath === "/experience" && "border-2 border-black"))}>
                   
-                  {/* <ul className="list-none ">
-                    <li key="1">
-                      
-                    </li>
-                    <li key="2">
-                      <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4")} href="/work?type=internships">
-                        internships
+                  {
+                    pathWithQuery === "/experience?type=education" ?
+                    "education" :
+                    (
+                      pathWithQuery === "/experience?type=work" ?
+                      "work" :
+                      (
+                        pathWithQuery === "/experience?type=positions" ?
+                        "extracurricular":
+                        "experience"
+                      )
+                    )
+
+                  }
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="p-4 bg-white shadow-lg rounded-md">
+                  <ul className=''>
+                    <li>
+                      <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=education" && "border-2 border-black")} href="/experience?type=education">
+                        education
                       </NavigationMenuLink>
-                    </li> */}
+                    </li>
+                    <li>
+                      <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=work" && "border-2 border-black")} href="/experience?type=work">
+                        work  
+                      </NavigationMenuLink>
+                    </li>
+                    <li key="3">
+                      <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", pathWithQuery === "/experience?type=positions" && "border-2 border-black")} href="/experience?type=positions">
+                        extracurricular
+                      </NavigationMenuLink>
+                    </li>
+                  </ul>
+                </NavigationMenuContent>
+                    {/* <div className='flex items-center'>
+                      <span className='text-sm text-left block'>work</span>
+                      <Separator className='ml-2' />
+                    </div> */}
+                    {/* <span className='text-sm pl-2'>work <Separator/></span> */}
+                    
+                    {/* <ul className="list-none ">
+                      <li key="1">
+                        
+                      </li>
+                      <li key="2">
+                        <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4")} href="/work?type=internships">
+                          internships
+                        </NavigationMenuLink>
+                      </li> */}
 
-                  {/* </ul>
-                </li>
+                    {/* </ul>
+                  </li>
 
-                
-              </ul>
-             */}
-              {/* <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4")} href="/work">
-                work
-              </NavigationMenuLink> */}
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/project" && "border-2 border-black")} href="/project">
-                projects
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/contact" && "border-2 border-black")} href="/contact">
-                contact
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu>
+                  
+                </ul>
+              */}
+                {/* <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4")} href="/work">
+                  work
+                </NavigationMenuLink> */}
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/project" && "border-2 border-black")} href="/project">
+                  projects
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink className={cn(navigationMenuTriggerStyle(), "text-lg py-4", currentPath === "/contact" && "border-2 border-black")} href="/contact">
+                  contact
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
         
         
         <div className=''>
